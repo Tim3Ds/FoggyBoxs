5009c0a3140db202910ad5de3ae0009d7506de46
Tim <Tim3Ds@gmail.com>
2017-03-28 09:52:37 -0600

    dot

--- src/Canvas/Dot/Dot.js
+++ src/Canvas/Dot/Dot.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 
 <Circle
-                            x={200} y={100} radius={30}
-                            fill='black'
-                            strokeWidth={this.state.isMouseInside ? 5 : 1}
-                            onMouseEnter={this.handleMouseEnter}
-                            onMouseLeave={this.handleMouseLeave}
-                        />
+    x={200} y={100} radius={30}
+    fill='black'
+    strokeWidth={this.state.isMouseInside ? 5 : 1}
+    onMouseEnter={this.handleMouseEnter}
+    onMouseLeave={this.handleMouseLeave}
+/>
