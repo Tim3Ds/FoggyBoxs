da70f12ca0ba740c871baa0e89f3099ef0778bb1
Tim3Ds <Tim3Ds@gmail.com>
2017-04-22 13:43:51 -0600

    loading game page and home page

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from '../Canvas';
+// import Form from '../Form';
+import Home from './Body';
 import {
   BrowserRouter as Router,
   Route,
@@ -18,37 +21,32 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Link to='/'><img src={logo} className="App-logo" alt="logo" /></Link>
-          <nav className="navigation">
-            <div className="nav-toggle" href="#">
-              <span className="trigger">&#9776;</span>
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/Game" activeStyle={{ background: 'white' }}>Game</Link></li>
-                <li><Link to="/Setup" activeStyle={{ background: 'white' }}>Setup</Link></li>
-              </ul>
-            </div>
-          </nav>
-        </header>
-        <section className="App-body" >
-          {this.props.children}
-        </section>
-        <footer className="App-footer">
-          <span className='Header'>Welcome to Foggy Boxes <span className='react'>a React App</span></span>
-        </footer> 
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to='/'><img src={logo} className="App-logo" alt="logo" /></Link>
+            <nav className="navigation">
+              <div className="nav-toggle" href="#">
+                <span className="trigger">&#9776;</span>
+                <ul>
+                  <li><Link to="/Home">Home</Link></li>
+                  <li><Link to="/Game/4/4" activeStyle={{ background: 'white' }}>Game</Link></li>
+                  <li><Link to="/Setup" activeStyle={{ background: 'white' }}>Setup</Link></li>
+                </ul>
+              </div>
+            </nav>
+          </header>
+          <section className="App-body" >
+            <Route path='/Home' component={Home} />
+            <Route path='/Game/:x/:y' component={Game} />
+          </section>
+          <footer className="App-footer">
+            <span className='Header'>Welcome to Foggy Boxes <span className='react'>a React App</span></span>
+          </footer> 
+        </div>
+      </Router>
     );
   }
 }
 
-const MainApp = () => (
-  <Router>
-    <Route component={App} />
-    
-  </Router>
-)
-
-
-export default MainApp;
+export default App;
